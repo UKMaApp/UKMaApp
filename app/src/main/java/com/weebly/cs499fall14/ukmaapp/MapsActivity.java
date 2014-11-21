2a5cbdf894d20a3375f7dc5726edb6b1dffe4dca
AndrewShields <alsh227@g.uky.edu>
2014-11-21 13:13:37 -0500

    Markers working for 2 buildings Camera tilted 30 degrees Code organization and style updated Overlay code included but not working (commented out)

--- app/src/main/java/com/weebly/cs499fall14/ukmaapp/MapsActivity.java
+++ app/src/main/java/com/weebly/cs499fall14/ukmaapp/MapsActivity.java
@@ -2,13 +2,14 @@ package com.weebly.cs499fall14.ukmaapp;
 
 import java.io.IOException;
 import java.util.List;
-
 import android.content.Context;
+import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -22,11 +23,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements LocationListener, LocationSource {
-
+public class MapsActivity extends FragmentActivity implements LocationListener, LocationSource
+{
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private LocationSource.OnLocationChangedListener mListener;
     private LocationManager locationManager;
@@ -38,96 +45,25 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        //For ping location
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if(locationManager != null)
-        {
-            boolean gpsIsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            boolean networkIsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if(gpsIsEnabled)
-            {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000L, 10F, this);
-            }
-            else if(networkIsEnabled)
-            {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000L, 10F, this);
-            }
-            else
-            {
-                //Show some kind of message that tells the user that the GPS is disabled.
-            }
-        }
-        else
-        {
-            //Show some generic error dialog because something must have gone wrong with location manager.
-        }
-        //LocationListener locLister = new MyLocationLister();
-        //locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locLister);
-
-        SupportMapFragment supportMapFragment = (SupportMapFragment)
-        getSupportFragmentManager().findFragmentById(R.id.map);
-        setUpMapIfNeeded();// Getting a reference to the map
-        mMap = supportMapFragment.getMap();
-
-        // Getting reference to btn_find of the layout activity_main
-        Button btn_find = (Button) findViewById(R.id.btn_find);
-
-        // Defining button click event listener for the find button
-        OnClickListener findClickListener = new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Getting reference to EditText to get the user input location
-                EditText etLocation = (EditText) findViewById(R.id.et_location);
-
-                // Getting user input location
-                String location = etLocation.getText().toString();
-
-                if(location!=null && !location.equals("")){
-                    new GeocoderTask().execute(location);
-                }
-            }
-        };
-        // Setting button click event listener for the find button
-        btn_find.setOnClickListener(findClickListener);
-    }
-
-    @Override
-    public void onPause()
-    {
-        if(locationManager != null)
-        {
-            locationManager.removeUpdates(this);
-        }
-
-        super.onPause();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
         setUpMapIfNeeded();
-        if(locationManager != null)
-        {
-            mMap.setMyLocationEnabled(true);
-        }
+        //setUpGroundOverlay();
+        setUpInfoWindows();
     }
 
+    private void setUpGroundOverlay() {
+        /* //Currently broken*
+        int resId = this.getResources().getIdentifier("overlay_bmp.bmp", "drawable", this.getPackageName());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
-    //sets 3D buildings on if "true"
-    public final void setBuildingsEnabled (){
+        LatLngBounds campusBounds = new LatLngBounds(
+                new LatLng(38.0444, -84.4949),       // South west corner
+                new LatLng(38.0094, -84.5173));      // North east corner
+        GroundOverlay campusMap = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.overlay_png))
+                .positionFromBounds(campusBounds));/**/
     }
 
     // An AsyncTask class for accessing the GeoCoding Web Service
-    private class GeocoderTask extends AsyncTask<String, Void, List<Address>>{
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
 
         @Override
         protected List<Address> doInBackground(String... locationName) {
@@ -146,8 +82,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
         @Override
         protected void onPostExecute(List<Address> addresses) {
-
-            if(addresses==null || addresses.size()==0){
+            if (addresses==null || addresses.size()==0) {
                 Toast.makeText(getBaseContext(), "No Location found", Toast.LENGTH_SHORT).show();
             }
 
@@ -155,8 +90,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
             mMap.clear();
 
             // Adding Markers on Google Map for each matching address
-            for(int i=0;i<addresses.size();i++){
-
+            for (int i=0; i<addresses.size(); i++) {
                 Address address = (Address) addresses.get(i);
 
                 // Creating an instance of GeoPoint, to display in Google Map
@@ -179,9 +113,56 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
                 mMap.addMarker(markerOptions);
 
                 // Locate the first location and centers
-                if(i==0)
+                if (i==0) {
                     mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                }
             }
+        }
+    }
+
+    private void setUpInfoWindows() {
+        Marker naturalScience = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(38.038021, -84.504692))
+                .title("Natural Science Building")
+                .alpha(0.0f) // 0.0 (invisible) - 1.0 (fully visible)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .snippet("http://www.google.com"));
+        naturalScience.showInfoWindow();
+
+        Marker POT = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(38.038681, -84.504191))
+                .title("Patterson Office Tower")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        POT.showInfoWindow();
+
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                String url = marker.getSnippet();
+
+                if (url != null) {
+                    Intent openUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(openUrl);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        if (locationManager != null) {
+            locationManager.removeUpdates(this);
+        }
+
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        if (locationManager != null) {
+            mMap.setMyLocationEnabled(true);
         }
     }
 
@@ -221,9 +202,61 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.setMyLocationEnabled(true);
         //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(38.038024, -84.504686), 18));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(38.038024, -84.504686), 18));
+
+        //For ping location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            boolean gpsIsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean networkIsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (gpsIsEnabled) {
+                //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10F, this);
+            } else if (networkIsEnabled) {
+                //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 10F, this);
+            } else {
+                //Show some kind of message that tells the user that the GPS is disabled.
+            }
+        } else {
+            //Show some generic error dialog because something must have gone wrong with location manager.
+        }
+        //LocationListener locLister = new MyLocationLister();
+        //locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locLister);
+
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.map);
+        setUpMapIfNeeded();// Getting a reference to the map
+        mMap = supportMapFragment.getMap();
+
+        // Getting reference to btn_find of the layout activity_main
+        Button btn_find = (Button) findViewById(R.id.btn_find);
+
+        // Defining button click event listener for the find button
+        OnClickListener findClickListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Getting reference to EditText to get the user input location
+                EditText etLocation = (EditText) findViewById(R.id.et_location);
+
+                // Getting user input location
+                String location = etLocation.getText().toString();
+
+                if (location!=null && !location.equals("")) {
+                    new GeocoderTask().execute(location);
+                }
+            }
+        };
+        // Setting button click event listener for the find button
+        btn_find.setOnClickListener(findClickListener);
+
+        // Set zoom and tilt
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(38.038024, -84.504686))
+                .zoom(18)
+                .tilt(30) // Sets the tilt of the camera to 30 degrees
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
         //added for locator button
         mMap.setMyLocationEnabled(true);
@@ -241,35 +274,40 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     }
 
     @Override
-    public void onLocationChanged(Location location)
-    {
-        if( mListener != null )
-        {
+    public void onLocationChanged(Location location) {
+        if( mListener != null ) {
             mListener.onLocationChanged( location );
-
             mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
         }
     }
 
     @Override
-    public void onProviderDisabled(String provider)
-    {
+    public void onProviderDisabled(String provider) {
         // TODO Auto-generated method stub
         Toast.makeText(this, "provider disabled", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onProviderEnabled(String provider)
-    {
+    public void onProviderEnabled(String provider) {
         // TODO Auto-generated method stub
         Toast.makeText(this, "provider enabled", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras)
-    {
+    public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
         Toast.makeText(this, "status changed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    //sets 3D buildings on if "true"
+    public final void setBuildingsEnabled (){
     }
 }
 
